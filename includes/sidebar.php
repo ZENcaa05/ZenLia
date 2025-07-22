b69260cd50649acb6ae62d1238ec460358dcd2d9
ZENcaa05 <muhammadhilmygg@gmail.com>
2025-07-22 10:00:05 +0700

    Delete includes directory

--- includes/sidebar.php
+++
@@ -1,8 +0,0 @@
-<div class="sidebar">
-  <h4 class="text-center">Nusantara Tech</h4>
-  <hr class="bg-white mx-3" />
-  <a href="dashboard.php">🏠 Dashboard</a>
-  <a href="#">👤 Profil</a>
-  <a href="#">⚙️ Pengaturan</a>
-  <a href="login.php">🚪 Logout</a>
-</div>
